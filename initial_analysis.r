729fa74925fdb8f66aeef6637d5b47409948197d
Beatrice Porta <76397227+bblankt@users.noreply.github.com>
2024-12-04 18:06:44 +0100

    Update initial_analysis.r
    ho aggiunto gli istogrammi ma non so se funzionano scritti così su R :( spero di aver messo le variabili con i nomi giusti

--- initial_analysis.r
+++ initial_analysis.r
@@ -1,4 +1,6 @@
 data <- read.csv("data/merged.csv")
+library(ggplot2)
+library(dplyr)
 
 n_commuters <- sum(data$is_commuter)
 total <- length(data$is_commuter)
@@ -19,3 +21,27 @@ boxplot(gpa[gpa != -1] ~ is_commuter[gpa != -1],
     xlab = "is_commuter",
     ylab = "gpa"
 )
+
+# Histograms for GPA (Commuters)
+filtered_commuters <- filter(data, is_commuter == 1 & gpa >= 0 & gpa <= 30)
+ggplot(filtered_commuters, aes(x = gpa)) +
+  geom_histogram(bins = 15, fill = "blue", color = "black", alpha = 0.7) +
+  geom_density(aes(y = ..density..), color = "red") + 
+  xlim(0, 30) +  
+  ggtitle('Histogram of GPA for Commuters') +
+  xlab('GPA') +
+  ylab('Frequency') +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+# Histogram for GPA (Non-commuters)
+filtered_non_commuters <- filter(data, is_commuter == 0 & gpa >= 0 & gpa <= 30)
+ggplot(filtered_non_commuters, aes(x = gpa)) +
+  geom_histogram(bins = 15, fill = "green", color = "black", alpha = 0.7) +
+  geom_density(aes(y = ..density..), color = "red") + 
+  xlim(0, 30) +  
+  ggtitle('Histogram of GPA for Non-commuters') +
+  xlab('GPA') +
+  ylab('Frequency') +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
